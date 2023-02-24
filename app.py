71a426554d6190a478a64ccd8f4734f7b54534d1
ghasempouri1984 <ash.mathcom@gmail.com>
2023-02-24 12:08:48 +0100

    changes during talk with prof.daquino

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import json
 import plotly.express as px
 from SPARQLWrapper import SPARQLWrapper, JSON
 from flask import Flask, request, Response
+import client
 
 app = Flask(__name__)
 
@@ -11,41 +12,10 @@ def plot():
     req_data = request.get_json()
     query = req_data.get('query')
     chart_type = req_data.get('chart_type', 'bar')
+    format = req_data.get('format', 'html')
 
     # Get SPARQL endpoint from JSON payload, default to Wikidata if not specified
     endpoint = req_data.get('endpoint', 'https://query.wikidata.org/sparql')
-
-    # Set up SPARQL query
-    sparql = SPARQLWrapper(endpoint)
-    sparql.setQuery(query)
-    sparql.setTimeout(60)  # Set timeout to 60 seconds
-    sparql.setReturnFormat(JSON)
-    results = sparql.query().convert()
-
-    # Extract data from results
-    data = results["results"]["bindings"]
-    count = data[0]["count"]["value"]
-    
-    # Get custom labels from query result, default to 'Count' if not specified
-    label_x = data[0]["label_x"]["value"] if "label_x" in data[0] else 'Count'
-    label_y = data[0]["label_y"]["value"] if "label_y" in data[0] else 'Count'
-
-    # Create chart based on chart type
-    if chart_type == 'bar':
-        fig = px.bar(x=[label_x], y=[count], labels={'x':'', 'y':label_y})
-    elif chart_type == 'pie':
-        fig = px.pie(values=[count], names=[label_x])
-    else:
-        return 'Invalid chart type', 400
-
-    # Get format from JSON payload, default to HTML if not specified
-    format = req_data.get('format', 'html')
-
-    # Return chart in the requested format
-    if format == 'html':
-        return fig.to_html()
-    elif format == 'png':
-        png_image = fig.to_image(format='png')
-        return Response(png_image, mimetype='image/png')
-    else:
-        return 'Invalid format', 400
+    resp=client.plotChart(query, chart_type, endpoint, format)
+    #according to format we return different response
+    return resp
--- client.py
+++ client.py
@@ -1,31 +1,60 @@
 import requests
 import json
 
-url = 'http://localhost:5000/plot'
+def plotChart(query, chart_type, endpoint, format):
+    var_resp=None
+    #url = 'http://localhost:5000/plot'
+    # Set up SPARQL query
+    sparql = SPARQLWrapper(endpoint)
+    sparql.setQuery(query)
+    sparql.setTimeout(60)  # Set timeout to 60 seconds
+    sparql.setReturnFormat(JSON)
+    results = sparql.query().convert()
 
-query = """
-    SELECT (COUNT(?human) AS ?count)
-    WHERE {
-        ?human wdt:P31 wd:Q5 .
-    }
-"""
+    # Extract data from results
+    data = results["results"]["bindings"]
 
-params = {
-    'query': query,
-    'chart_type': 'bar',
-    'endpoint': 'https://query.wikidata.org/sparql',
-    'format': 'png'
-}
+    # Create chart based on chart type
+    if chart_type == 'bar':
+        count = data[0]["count"]["value"]
+    
+        # Get custom labels from query result, default to 'Count' if not specified
+        label_x = data[0]["label_x"]["value"] if "label_x" in data[0] else 'Count'
+        label_y = data[0]["label_y"]["value"] if "label_y" in data[0] else 'Count'
+        fig = px.bar(x=[label_x], y=[count], labels={'x':'', 'y':label_y})
 
-response = requests.post(url, json=params)
-#print(response.text)
-if params['format'] == 'html':
-    with open('output2.html', 'wb') as file:
-        #file.write(response.text)
-        #file.write(response.text.encode('utf-8', 'ignore').decode('utf-8'))
-        file.write(response.text.encode('utf-8'))
+    elif chart_type == 'pie':
+        fig = px.pie(values=[count], names=[label_x])
+    else:
+    #    return 'Invalid chart type', 400
+        var_resp= 'Invalid chart type'
 
-elif params['format'] == 'png':
-    with open('chart.png', 'wb') as file:
-        file.write(response.content)
+    # Get format from JSON payload, default to HTML if not specified
+    #format = req_data.get('format', 'html')
 
+    # Return chart in the requested format
+    if format == 'html':
+        #return fig.to_html()
+        var_resp=fig.to_html()
+    elif format == 'png':
+        var_resp = fig.to_image(format='png')
+        #return Response(png_image, mimetype='image/png')
+
+    else:
+        var_resp='Invalid format'
+
+    #response = requests.post(url, json=params)
+    #print(response.text)
+    if params['format'] == 'html':
+        with open('output2.html', 'wb') as file:
+            #file.write(response.text)
+            #file.write(response.text.encode('utf-8', 'ignore').decode('utf-8'))
+            #file.write(response.text.encode('utf-8'))
+            file.write(var_resp.text.encode('utf-8'))
+            
+    elif params['format'] == 'png':
+        with open('chart.png', 'wb') as file:
+            #file.write(response.content)
+            file.write(var_resp.content)
+    return var_resp
+    
