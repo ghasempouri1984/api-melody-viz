c63c294fd93b454c4e40301b32981c61f4a74482
ghasempouri1984 <ash.mathcom@gmail.com>
2023-03-09 17:56:42 +0100

    ...

--- client.py
+++ client.py
@@ -1,10 +1,11 @@
 import requests
 import json
+from SPARQLWrapper import SPARQLWrapper, JSON
+import plotly.express as px
+
 
 def plotChart(query, chart_type, endpoint, format):
     var_resp=None
-    #url = 'http://localhost:5000/plot'
-    # Set up SPARQL query
     sparql = SPARQLWrapper(endpoint)
     sparql.setQuery(query)
     sparql.setTimeout(60)  # Set timeout to 60 seconds
@@ -17,7 +18,6 @@ def plotChart(query, chart_type, endpoint, format):
     # Create chart based on chart type
     if chart_type == 'bar':
         count = data[0]["count"]["value"]
-    
         # Get custom labels from query result, default to 'Count' if not specified
         label_x = data[0]["label_x"]["value"] if "label_x" in data[0] else 'Count'
         label_y = data[0]["label_y"]["value"] if "label_y" in data[0] else 'Count'
@@ -26,35 +26,15 @@ def plotChart(query, chart_type, endpoint, format):
     elif chart_type == 'pie':
         fig = px.pie(values=[count], names=[label_x])
     else:
-    #    return 'Invalid chart type', 400
         var_resp= 'Invalid chart type'
 
-    # Get format from JSON payload, default to HTML if not specified
-    #format = req_data.get('format', 'html')
-
-    # Return chart in the requested format
     if format == 'html':
-        #return fig.to_html()
         var_resp=fig.to_html()
     elif format == 'png':
         var_resp = fig.to_image(format='png')
-        #return Response(png_image, mimetype='image/png')
-
     else:
         var_resp='Invalid format'
 
-    #response = requests.post(url, json=params)
-    #print(response.text)
-    if params['format'] == 'html':
-        with open('output2.html', 'wb') as file:
-            #file.write(response.text)
-            #file.write(response.text.encode('utf-8', 'ignore').decode('utf-8'))
-            #file.write(response.text.encode('utf-8'))
-            file.write(var_resp.text.encode('utf-8'))
-            
-    elif params['format'] == 'png':
-        with open('chart.png', 'wb') as file:
-            #file.write(response.content)
-            file.write(var_resp.content)
+
     return var_resp
     
