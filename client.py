4916248a5127782dee0942b2dff739d82d8754f0
ghasempouri1984 <ash.mathcom@gmail.com>
2023-03-22 12:23:28 +0100

    Update client.py

--- client.py
+++ client.py
@@ -14,17 +14,23 @@ def plotChart(query, chart_type, endpoint, format):
 
     # Extract data from results
     data = results["results"]["bindings"]
-
+    count = data[0]["count"]["value"]
+    # Get custom labels from query result, default to 'Count' if not specified
+    label_x = data[0]["label_x"]["value"] if "label_x" in data[0] else 'Count'
+    label_y = data[0]["label_y"]["value"] if "label_y" in data[0] else 'Count'
     # Create chart based on chart type
     if chart_type == 'bar':
-        count = data[0]["count"]["value"]
-        # Get custom labels from query result, default to 'Count' if not specified
-        label_x = data[0]["label_x"]["value"] if "label_x" in data[0] else 'Count'
-        label_y = data[0]["label_y"]["value"] if "label_y" in data[0] else 'Count'
-        fig = px.bar(x=[label_x], y=[count], labels={'x':'', 'y':label_y})
+        
+        #fig = px.bar(x=[label_x], y=[count], labels={'x':'', 'y':label_y})
+        fig = px.bar(x=[label_x], y=[count], labels={'x':'', 'y':label_y}, title="Number of items", height=500, width=500)
+        
+
 
     elif chart_type == 'pie':
-        fig = px.pie(values=[count], names=[label_x])
+        #fig = px.pie(values=[count], names=[label_x])
+        counts = [entry["count"]["value"] for entry in data]
+        labels = [entry["label_x"]["value"] for entry in data]
+        fig = px.pie(values=counts, names=labels)
     else:
         var_resp= 'Invalid chart type'
 
@@ -35,7 +41,6 @@ def plotChart(query, chart_type, endpoint, format):
         
     else:
         var_resp='Invalid format'
-
 
     return var_resp
     
